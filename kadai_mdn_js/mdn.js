7947035b120d6db2fd403741ea44caa93ea00647
T-Hiropy <akidokimee53@yahoo.co.jp>
2024-12-15 20:34:18 +0900

    Add kadai_mdn_js

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,9 @@
+// 現在時刻表示
+var nowDate = new Date();
+var nowYYYY = nowDate.getFullYear();
+var nowMM = nowDate.getMonth();
+var nowDD = nowDate.getDate();
+
+// 出力
+// console.log(nowDate);
+console.log(nowYYYY +`年`+ nowMM + `月` + nowDD +`日`);
